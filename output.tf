f6eed60faafd35b8bc43a8c8a28b9b4fe4d4afee
Corey Callaway <coreylcallaway@gmail.com>
2022-05-23 14:05:34 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,6 +2,35 @@
 # This module allows the creation of a Virtual Network
 ##############################################################
 
+output "exist_vnet_name" {
+  description = "Virtual network data object."
+  value       = azurerm_virtual_network.exist_vnet.name
+}
+
+output "fe_sn_id" {
+  description = "Subnet data object."
+  value       = azurerm_subnet.exist_sn_fe.id
+}
+
+output "aks_sn_id" {
+  description = "Subnet data object."
+  value       = azurerm_subnet.exist_sn_aks.id
+}
+  
+output "route_tables" {
+  description = "Maps of custom route tables."
+  value = { for k, v in var.route_tables :
+    k => {
+      name    = azurerm_route_table.route_table[k].name
+      id      = azurerm_route_table.route_table[k].id
+      subnets = azurerm_route_table.route_table[k].subnets
+    }
+  }
+}
+
+/*
+
+
 output "vnet" {
   description = "Virtual network data object."
   value       = azurerm_virtual_network.vnet
@@ -85,13 +114,4 @@ output "aks" {
   }
 }
 
-output "route_tables" {
-  description = "Maps of custom route tables."
-  value = { for k, v in var.route_tables :
-    k => {
-      name    = azurerm_route_table.route_table[k].name
-      id      = azurerm_route_table.route_table[k].id
-      subnets = azurerm_route_table.route_table[k].subnets
-    }
-  }
-}
+*/
